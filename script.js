06cc4beb6f2590c1f3c331721a2f94a8b00b7e87
netomilfont <netomilfont@hotmail.com>
2022-06-14 13:48:40 -0300

    Praticando com variaveis em JS

---
+++ script.js
@@ -0,0 +1,9 @@
+let nome = 'Jhon'
+let sobrenome = 'Doyle'
+let anoNascimento = 1991
+let anoAtual = 2022
+let idade = anoAtual - anoNascimento
+let cidade = 'Curitiba'
+
+console.log(`Eu sou o ${nome} ${sobrenome} tenho ${idade} anos e moro em ${cidade}.`)
+
